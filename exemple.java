2e4bc3a728ad012eb675c53464114e3102d99b3a
dani1001 <39743276+dani1001@users.noreply.github.com>
2018-06-11 10:59:48 +0200

    Update exemple.java

--- exemple.java
+++ exemple.java
@@ -3,5 +3,6 @@ public class HelloWorld {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
       System.out.println("Hello, World 2");
+      System.out.println("Hello, World 3");
    }
 }
