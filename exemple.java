aae07aa070c0e348d9ce70c96af45db6bd0de36b
dani1001 <39743276+dani1001@users.noreply.github.com>
2018-06-11 11:38:25 +0200

    Update exemple.java

--- exemple.java
+++ exemple.java
@@ -8,5 +8,6 @@ public class HelloWorld {
       System.out.println("Hello, World 5");
       System.out.println("Hello, World 6");
       System.out.println("Hello, World 7");
+      System.out.println("Hello, World 8");
    }
 }
