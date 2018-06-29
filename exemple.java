7f2119c44d154587514bbe45f94cfac4dde3f981
dani1001 <39743276+dani1001@users.noreply.github.com>
2018-06-29 18:44:31 +0200

    Add files via upload

---
+++ exemple.java
@@ -0,0 +1,9 @@
+public class exemple {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+      System.out.println("Hello, World 2");
+      System.out.println("Hello, World 3");
+      System.out.println("Hello, World 4");
+   }
+}
