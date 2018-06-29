2b9c7cbb1c87c56f6aa2b89e3f10d9ccf50e334e
dani1001 <39743276+dani1001@users.noreply.github.com>
2018-06-29 18:43:12 +0200

    Delete exemple.java

--- src/exemple.java
+++
@@ -1,9 +0,0 @@
-public class exemple {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-      System.out.println("Hello, World 2");
-      System.out.println("Hello, World 3");
-      System.out.println("Hello, World 4");
-   }
-}
